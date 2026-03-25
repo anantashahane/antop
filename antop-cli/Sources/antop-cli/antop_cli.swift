522d353ac5ce78c4bd0a4f2e2f08f8c8a9f402fa
Ananta Shahane <ananta.shahane@aol.com>
2026-03-25 13:38:46 +0100

    Circlular buffer added.

--- antop-cli/Sources/antop-cli/antop_cli.swift
+++ antop-cli/Sources/antop-cli/antop_cli.swift
@@ -3,27 +3,32 @@
 import Foundation
 
 @main
-@available(macOS 12, *)
+@available(macOS 13, *)
 struct SwiftExecutable {
     static func main() async {
         var stats = Statistics()
+        let clock = ContinuousClock()
+
         print("Starting antop stream…")
+        
         for await line in StreamPowerBlocks() {
+        let eta = clock.measure {
             CaptureMachineName(from: line, into: &stats)
             CaptureEfficiencyCore(from: line, into: &stats)
             CapturePerformanceCore(from: line, into: &stats)
             CaptureGPU(from: line, into: &stats)
             CapturePower(from: line, into: &stats)
+        }
             print("---------------------------------------------")
             print(stats.machineName ??  "Unknown")
             print("E-Core Cluster: \(stats.highEffeciencyUtility)% @ \(stats.highEffeciencyFrequency) MHz")
             print("P-Core Cluster: \(stats.highPerformanceUtility)% @ \(stats.highPerformanceFrequency) MHz")
             print("GPU Usage: \(stats.gpuUtility)% @ \(stats.gpuFrequency) MHz")
-            print("  CPU Power: \(stats.CPUPower) mW")
-            print("+ GPU Power: \(stats.GPUPower) mW")
-            print("+ ANE Usage: \(stats.ANEPower) mW")
+            print("  CPU Power: \(stats.CPUPower.get().last ?? 0) mW; average \(stats.CPUPower.getAverage()) mW")
+            print("+ GPU Power: \(stats.GPUPower.get().last ?? 0) mW; average \(stats.GPUPower.getAverage()) mW")
+            print("+ ANE Usage: \(stats.ANEPower.get().last ?? 0) mW; average \(stats.ANEPower.getAverage()) mW")
             print("---------------------------------------------")
-            print("Package Power: \(stats.PackagePower) mW")
+            print("Package Power: \(stats.PackagePower) mW; average \(stats.PackagePower.getAverage()) mW; peak \(stats.PackagePower.getMax()); took \(eta).")
             print("---------------------------------------------")
         }
     }
--- antop-cli/Sources/antop-cli/powermetrics.swift
+++ antop-cli/Sources/antop-cli/powermetrics.swift
@@ -1,5 +1,84 @@
 import Foundation
 
+enum CaptureRegex {
+    static let machineModel = regex(#"^Machine model: (.*)"#)
+
+    static let cpuPower = regex(#"CPU Power:\s+([0-9]+)"#)
+    static let gpuPower = regex(#"GPU Power:\s+([0-9]+)"#)
+    static let anePower = regex(#"ANE Power:\s+([0-9]+)"#)
+    static let packagePower = regex(#"Combined Power \(CPU \+ GPU \+ ANE\):\s([0-9]+)"#)
+
+    static let efficiencyCoreFrequency = regex(#"E-Cluster HW active frequency: ([0-9]+)"#)
+    static let efficiencyCoreActivity = regex(#"E-Cluster HW active residency:\s+([0-9.]+)"#)
+    static let performanceCoreFrequency = regex(#"P-Cluster HW active frequency: ([0-9]+)"#)
+    static let performanceCoreActivity = regex(#"P-Cluster HW active residency:\s+([0-9.]+)"#)
+    
+    static let gpuFrequency = regex(#"GPU HW active frequency:\s+([0-9])"#)
+    static let gpuActivity = regex(#"GPU HW active residency:\s+([0-9])"#)
+    
+
+
+
+    private static func regex(_ pattern: String) -> NSRegularExpression {
+        try! NSRegularExpression(pattern: pattern)
+    }
+}
+
+class LimitedArray<T: Comparable & Numeric> : CustomStringConvertible {
+    var description: String {
+        return "\(self.get())"
+    }
+    private var max : T? = nil
+    private let size : Int
+    private var pointer : Int
+    private var data : Array<T>
+
+    init(size: Int) {
+        precondition(size > 0, "Size must be > 0")
+        self.size = size
+        self.pointer = 0
+        self.data = Array<T>()
+        self.data.reserveCapacity(size)
+    }
+
+    func append(_ element : T) {
+        if let max = self.max {
+            if max < element {
+                self.max = element
+            }
+        } else {
+            self.max = element
+        }
+        if self.data.count < self.size {
+            self.data.append(element)
+        } else {
+            self.data[pointer] = element
+        }
+        self.pointer  = (self.pointer + 1) % self.size
+    }
+
+    func get() -> Array<T> {
+        if self.data.count < self.size {
+            return self.data
+        }
+        return Array(self.data[self.pointer...] + self.data[0..<self.pointer])
+    }
+
+    func getAverage() -> Double where T: Numeric {
+        guard !self.data.isEmpty else { return 0 }
+        if let floatingData = data as? [Double] {
+            return floatingData.reduce(0, +) / Double(floatingData.count)
+        } else if let integerData = data as? [Int] {
+            return Double(integerData.reduce(0, +)) / Double(integerData.count)
+        }
+        return 0
+    }
+
+    func getMax() -> T {
+        return self.max ?? T.zero
+    }
+}
+
 struct Statistics {
     var machineName: String?
 
@@ -16,16 +95,15 @@ struct Statistics {
     var gpuUtility: Double = 0
 
     // Power
-    var CPUPower : Int = 0
-    var GPUPower : Int = 0
-    var PackagePower: Int = 0
-    var ANEPower : Int = 0
+    var CPUPower = LimitedArray<Int>(size: 30)
+    var GPUPower = LimitedArray<Int>(size: 30)
+    var PackagePower = LimitedArray<Int>(size: 30)
+    var ANEPower = LimitedArray<Int>(size: 30)
 }
 
 import Foundation
 
-func capture(_ pattern: String, in line: String) -> String? {
-    guard let regex = try? NSRegularExpression(pattern: pattern) else { return nil }
+func capture(_ regex: NSRegularExpression, in line: String) -> String? {
     let range = NSRange(line.startIndex..., in: line)
     
     guard let match = regex.firstMatch(in: line, range: range),
@@ -37,52 +115,52 @@ func capture(_ pattern: String, in line: String) -> String? {
 }
 
 func CaptureMachineName(from line: String, into stats: inout Statistics) {
-    if let name = capture(#"^Machine model: (.*)"#, in: line) {
+    if let name = capture(CaptureRegex.machineModel, in: line) {
         stats.machineName = name
     }
 }
 
 func CapturePower(from line: String, into stats: inout Statistics) {
-    if let CPUPower = capture(#"CPU Power:\s+([0-9]+)"#, in: line) {
-        stats.CPUPower = Int(CPUPower) ?? stats.CPUPower
+    if let CPUPower = capture(CaptureRegex.cpuPower, in: line) {
+        stats.CPUPower.append(Int(CPUPower) ?? 0)
     }
-    if let GPUPower = capture(#"GPU Power:\s+([0-9]+)"#, in: line) {
-        stats.GPUPower = Int(GPUPower) ?? stats.GPUPower
+    if let GPUPower = capture(CaptureRegex.gpuPower, in: line) {
+        stats.GPUPower.append(Int(GPUPower) ?? 0)
     }
-    if let ANEPower = capture(#"ANE Power:\s+([0-9]+)"#, in: line) {
-        stats.ANEPower = Int(ANEPower) ?? stats.ANEPower
+    if let ANEPower = capture(CaptureRegex.anePower, in: line) {
+        stats.ANEPower.append(Int(ANEPower) ?? 0)
     }
-    if let PackagePower = capture(#"Combined Power \(CPU \+ GPU \+ ANE\):\s([0-9]+)"#, in: line) {
-        stats.PackagePower = Int(PackagePower) ?? stats.PackagePower
+    if let PackagePower = capture(CaptureRegex.packagePower, in: line) {
+        stats.PackagePower.append(Int(PackagePower) ?? 0)
     }
 }
 
 func CaptureEfficiencyCore(from line: String, into stats: inout Statistics) {
-    if let freq = capture(#"E-Cluster HW active frequency: ([0-9]+)"#, in: line) {
+    if let freq = capture(CaptureRegex.efficiencyCoreFrequency, in: line) {
         stats.highEffeciencyFrequency = Int(freq) ?? stats.highEffeciencyFrequency
     }
 
-    if let util = capture(#"E-Cluster HW active residency:\s+([0-9.]+)"#, in: line) {
+    if let util = capture(CaptureRegex.efficiencyCoreActivity, in: line) {
         stats.highEffeciencyUtility = Double(util) ?? stats.highEffeciencyUtility
     }
 }
 
 func CapturePerformanceCore(from line: String, into stats: inout Statistics) {
-    if let freq = capture(#"P-Cluster HW active frequency: ([0-9]+)"#, in: line) {
+    if let freq = capture(CaptureRegex.performanceCoreFrequency, in: line) {
         stats.highPerformanceFrequency = Int(freq) ?? stats.highPerformanceFrequency
     }
 
-    if let util = capture(#"P-Cluster HW active residency:\s+([0-9.]+)"#, in: line) {
+    if let util = capture(CaptureRegex.performanceCoreActivity, in: line) {
         stats.highPerformanceUtility = Double(util) ?? stats.highPerformanceUtility
     }
 }
 
 func CaptureGPU(from line: String, into stats: inout Statistics) {
-    if let gpuFrequency = capture(#"GPU HW active frequency:\s+([0-9])"#, in: line) {
+    if let gpuFrequency = capture(CaptureRegex.gpuFrequency, in: line) {
         stats.gpuFrequency = Int(gpuFrequency) ?? stats.gpuFrequency
     }
 
-    if let gpuUtility = capture(#"GPU HW active residency:\s+([0-9])"#, in: line) {
+    if let gpuUtility = capture(CaptureRegex.gpuActivity, in: line) {
         stats.gpuUtility = Double(gpuUtility) ?? stats.gpuUtility
     }
 }
@@ -101,6 +179,7 @@ func StreamPowerBlocks() -> AsyncStream<String> {
                         buffer.removeAll()
                     }
                 }
+                buffer.append(line)
             }
 
             continuation.finish()
@@ -116,6 +195,9 @@ func streamPowerMetrics() -> AsyncStream<String> {
 
             let pipe = Pipe()
             process.standardOutput = pipe
+
+            let errPipe = Pipe()
+            process.standardError = errPipe
 
             do {
                 try process.run()
